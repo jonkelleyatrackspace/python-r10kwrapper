63207f26f392f04fcdd85701b769cfabdbd0081d
jon.kelley <jon.kelley@rackspace.com>
2014-10-02 11:40:46 -0500

    Adding a bunch of changes to support configuration files and other things now. Yay

--- r10kwrapper.py
+++ r10kwrapper.py
@@ -1,57 +1,129 @@
 #!/usr/bin/python
 
-import logging, argparse, sys, subprocess
+import logging, subprocess, ConfigParser, argparse, sys, os
 
-# Command Usage
-#  usage: r10kwrapper.py [-h] -p PUPPETFILE -m MODULE_DESTINATION [-x ACTION] [-v VERBOSITY] 
-#                        [-f FLAGS_APPEND]
-# Where
-#
-#  -x ACTION......... : Selects an action to pass to r10k. Possible values:
-#                      [install] installs the modules from the puppetfile
-#                      [check] checks the syntax
-#                      [purge] purges modules
-#  -p PUPPETFILE..... : Provides the puppetfile you wish to load into r10k
-#  -m MOD_DESTINATION : Where the Puppetfile modules should be cloned under.
-#  -v VERBOSITY...... : Possible values of 0-3 where higher is more verbosity.
-#  -f FLAGS_APPEND... : Append additional flags to be passed at the end of the r10k command when wrapped.
-#  -h HELP........... : Unknown
+__author__  = 'Jonathan Kelley'
 
-import logging, argparse, sys, subprocess
-
-__author__ = 'Jonathan Kelley'
-appname     = 'r10kwrapper'
 r10k_binary = '/usr/bin/r10k'
 r10k_module = 'puppetfile'
 
-class ansi:
-    """ ANSI colors """
-    black  = '\033[30m'
-    blue   = '\033[34m'
-    green  = '\033[32m'
-    cyan   = '\033[36m'
-    red    = '\033[31m'
-    purple = '\033[35m'
-    brown  = '\033[33m'
-    gray   = '\033[37m'
-    clear  = '\033[0m'
+env = os.environ
+if env.get('DISPLAY') and env.get('I_HATE_ANSI') != '1':
+    """ Allows you to disable color with export ANSI=1
+    """
+    class ansi:
+        """ ANSI colors """
+        black  = '\033[30m'
+        blue   = '\033[34m'
+        green  = '\033[32m'
+        cyan   = '\033[36m'
+        red    = '\033[31m'
+        purple = '\033[35m'
+        brown  = '\033[33m'
+        gray   = '\033[37m'
+        attention = '\033[;4;5;34m'
+        clear  = '\033[0m'
+else:
+    class ansi:
+        """ ANSI colors """
+        black  = ''
+        blue   = ''
+        green  = ''
+        cyan   = ''
+        red    = ''
+        purple = ''
+        brown  = ''
+        gray   = ''
+        attention = ''
+        clear  = ''
+
+def enable_logging(level):
+    """ Enables logging at the level passed in. """
+    # Plenty of error types for this sort of app huh?
+    available_error_levels = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG ]
+    available_err_names = [ 'error', 'warning', 'info', 'debug']
+    fmt = (ansi.green + ' :::' + ansi.attention + '%(levelname)-8s' + ansi.red + 
+        '::: ' + ansi.clear +' %(message)s')
+
+    try:
+        errorlevel = available_error_levels[level]
+    except IndexError:
+        # Logging failed so lets assume control.
+        logging.basicConfig(level=logging.DEBUG,format=fmt)
+        logging.critical(ansi.red + "Verbosity levels can only be 0-" + 
+            str(len(available_error_levels)-1) + ' ' + str(available_err_names) + ansi.clear)
+        sys.exit(254)
+    logging.basicConfig(level=errorlevel,format=fmt)
+
+def determine_path_load_method(puppetfile,module_destination,configsection):
+    """ Split logic to ensure either dest&configsection defined
+        OR configsection defined as config load option. Exit if requirement
+        not met.
+
+        If that failure mode is not detected, ELIF statements determine the puppetfile
+        and module_destination paths based on the chosen method via argparse."""
+    if (configsection == None and module_destination == None or
+        configsection == None and puppetfile == None):
+        # User didn't provide a way for r10k to know puppetfile and modulepaths.
+        # Throw error to user and exit. Either be explicit or load from config.
+        logging.critical("You must provide the " + ansi.cyan + "-c" + ansi.clear + 
+            " parameter to load from config, \n" +  "\t\tor the " + ansi.cyan + 
+            " -p " + ansi.clear +  "and " + ansi.cyan + "-d " + ansi.clear + 
+            "parameters to statically reference your r10k configuration.\n" +
+            "\t\tUse " + ansi.cyan + "-h " + ansi.clear + "for more information.")
+        sys.exit(253)
+
+    elif configsection != None:
+        return retrieve_config_sections_from_disk(sections=configsection)
+
+    elif module_destination != None or puppetfile != None:
+        return [(puppetfile,module_destination),]
+
+def retrieve_config_sections_from_disk(inifile='r10kwrapper.conf',sections=[]):
+    """ Parses the inifile and returns the desired configuration sections.
+    """
+    config = ConfigParser.RawConfigParser()
+    config.read(inifile)
+
+    batch_result = []
+    if 'all' in sections:
+        for module, envvars in config._sections.items():
+            logging.debug(ansi.cyan + 'Found ' + ansi.green + module + ansi.cyan + 
+                " within ini file." + ansi.clear )
+
+            puppetfile = envvars['puppetfile']
+            module_destination = envvars['moduledest']
+
+            batch_result.append((puppetfile,module_destination))
+
+    else:
+        for section in sections:
+            puppetfile = config.items(section)[0][1]
+            module_destination = config.items(section)[1][1]
+
+            batch_result.append((puppetfile,module_destination))
+
+    return batch_result
 
 def execute_r10k(puppetfile=None,modules_directory=None,action="check",r10k_append_flags=''):
     """ Wraps some magic around the r10k command """
 
     if not action == "check":
+        # We really force check everything.
         execute_r10k(puppetfile=puppetfile,modules_directory=modules_directory,action="check",
             r10k_append_flags=r10k_append_flags)
 
     command = str(r10k_binary) + " " + str(r10k_module) + " " + str(action) + " " + str(r10k_append_flags)
-
-    logging.info(ansi.cyan + "R10K_ACTION=" + ansi.green + str(action) + " " + ansi.cyan +
-     "PUPPETFILE=" + ansi.green +puppetfile + ansi.cyan + " PUPPETFILE_DIR=" +
-      ansi.green + modules_directory + ansi.clear)
-
     env_vars = {'PUPPETFILE': puppetfile, 'PUPPETFILE_DIR': modules_directory}
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, env=env_vars)
-    output = process.communicate()[0]
+
+    logging.debug(ansi.brown + "ENV_VARS: " + str(env_vars) + ansi.clear)
+    logging.debug(ansi.brown + "SHELLEXEC: " + str(command) + ansi.clear)
+    process  = subprocess.Popen(command.split(), stdout=subprocess.PIPE, env=env_vars)
+    output   = process.communicate()[0]
+
+    logging.debug(ansi.brown + "STAT: " + str(process.returncode) + ansi.brown + " --- " +
+        "STDOUT: " + str(output) + ansi.clear)
+
     if process.returncode > 0:
         logging.critical( ansi.red + appname + ': r10k execution failed.' + ansi.clear)
         sys.exit(1)
@@ -60,36 +132,60 @@ def parse_arguements():
     """ Parses and returns arguements defined by the user. """
     # Assign description to the help doc
     parser = argparse.ArgumentParser(
-        description='Wrapper for r10k to ease management of the Rackspace Puppet r10k structure.')
-    # Add arguments
+        description='Wrapper for r10k to ease management of the Rackspace Puppet ' +
+        'r10k structure.')
+
     parser.add_argument(
-        '-p', '--puppetfile', type=str, help='Path to Puppetfile (ex. /etc/r10k/dev/modules-rax/Puppetfile', required=True)
+        '-p', '--puppetfile', type=str, help='Path to Puppetfile holding module ' + 
+        'dependecies.', required=False, default=None)
     parser.add_argument(
-        '-m', '--module_destination', type=str, help='Path to deploy modules under (ex. /etc/puppet/modules-rax/)', required=True)
+        '-d', '--dest', type=str, help='Path to deploy Puppetfile modules to.',
+        required=False, default=None)
     parser.add_argument(
-        '-x', '--action', type=str, help='Action to perform with Puppetfile (check,install,purge) supported.', required=False, default="check")
+        '-c', '--configsection', type=str, help='Loads r10k settings from a ini ' + 
+        'section. This arguement can be supplied multiple times. If ALL is supplied' +
+        ', all sections will be executed. (ex -i modules -i profiles -i signup -i ' + 
+        'cloudfeeds)', required=False, action='append', default=None)
     parser.add_argument(
-        '-v', '--verbosity', type=int, help='Log level (0 = error, 1 = warn, 2 = info, 3 = debug) (Default: 2)', required=False, default=2)
+        '-C', '--configfile', type=str, help='Defines the ini file to load. Otherwise ' +
+        'it assumes /etc/r10k/wrapper.conf', required=False, default='/etc/r10k/wrapper.conf')
     parser.add_argument(
-        '-f', '--flags_append', type=str, help='Pass trailing arguements to r10k command, (ex. -v debug)', required=False, default='')
+        '-x', '--action', type=str, help='Action to perform with Puppetfile ' + 
+        '(check,install,purge) supported.', required=True, default="check")
+    parser.add_argument(
+        '-v', '--verbosity', type=int, help='Log level (0 = error, 1 = warn, ' + 
+            '2 = info, 3 = debug) (Default: 3)', required=False, default=3)
+    parser.add_argument(
+        '-f', '--flags_append', type=str, help='Pass trailing arguements to r10k' + 
+        ' command, (ex. -v debug)', required=False, default='')
 
     args = parser.parse_args()
 
-    # verbosity input validation
-    errorlevel = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG ]
-    try:
-        errorlevel = errorlevel[args.verbosity]
-    except IndexError:
-        print (ansi.red + "ERROR: Verbosity levels can only be 0-" + str(len(errorlevel)-1) + ansi.clear)
-        sys.exit(254)
+    enable_logging(args.verbosity) # init logging
+    r10k_batch_list = determine_path_load_method(puppetfile=args.puppetfile,
+        module_destination=args.dest,
+        configsection=args.configsection)
 
-    logging.basicConfig(level=errorlevel)
-    ##
-
-    return args.puppetfile, args.module_destination, args.action, args.flags_append
+    return r10k_batch_list, args.action, args.flags_append
 
 if __name__ == "__main__":
     """ Run """
-    puppetfile, module_destination, action, flags_append = parse_arguements()
-    execute_r10k(puppetfile=puppetfile,modules_directory=module_destination, action=action, r10k_append_flags=flags_append)
+    r10k_action_batch, action, flags_append = parse_arguements()
 
+    num_actions = str(len(r10k_action_batch))
+    logging.info(ansi.cyan + ansi.green +  num_actions +
+     ansi.cyan + " batch actions gathered for execution" + ansi.clear )
+
+    statuscount = 0
+    for puppetfile, module_destination in r10k_action_batch:
+        statuscount += 1
+        logging.info( ansi.cyan + "Action " + ansi.green + str(statuscount) +
+            " of " + num_actions + ansi.brown + " -> " + ansi.cyan + 
+            "R10K ACTION=" + ansi.green + str(action) + " " + ansi.cyan +
+            "PUPPETFILE=" + ansi.green + puppetfile + ansi.cyan + 
+            " PUPPETFILE_DIR=" + ansi.green + module_destination + ansi.clear)
+
+        execute_r10k(puppetfile=puppetfile,
+            modules_directory=module_destination, 
+            action=action,
+            r10k_append_flags=flags_append)
